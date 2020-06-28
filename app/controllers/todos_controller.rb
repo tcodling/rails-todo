d5c541559663dc4ea583cd7ff0f6e5123e2e735f
tcodling <thomas.codling@outlook.com>
2020-06-28 22:19:11 +1200

    add ability to add new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,24 @@
 class TodosController < ApplicationController
     def index
+        @todo = Todo.new
         @todos = Todo.all
+    end
+
+    def new
+        @todo = Todo.new
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+        if @todo.save 
+            redirect_to '/' 
+        else 
+            render 'new' 
+        end 
+    end
+
+    private
+    def todo_params
+        params.require(:todo).permit(:task, :done)
     end
 end
