57b5f22398aec1584160eed49d806780dfd2eb01
tcodling <thomas.codling@outlook.com>
2020-07-05 18:37:14 +1200

    setup sessions controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+    before_action :require_user, only: [:index, :show]
+
     def index
         @lists = List.all
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
+        session[:user_id] = @user.id 
         if @user.save
             redirect_to '/'
         else
