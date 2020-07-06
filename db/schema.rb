68fc4bbf3b2cf6cac9f3b19df84356dc6f03b77f
tcodling <thomas.codling@outlook.com>
2020-07-06 13:27:00 +1200

    update user migration and seed

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema.define(version: 2020_07_05_023600) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "todos", force: :cascade do |t|
